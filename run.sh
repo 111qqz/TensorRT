d9c907270c6b9e95be10f3d6381207bb948a67de
renkuanze <renkuanze@sensetime.com>
2020-04-10 11:13:21 +0000

    use debug mode

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export TRT_RELEASE=/tensorrt
 cd build 
-cmake  -DCUDNN_ROOT_DIR=/workspace/cuDNN  -DTRT_LIB_DIR=$TRT_RELEASE/lib -DTRT_BIN_DIR=`pwd`/out   ..
+cmake -DCMAKE_BUILD_TYPE=Debug  -DCUDNN_ROOT_DIR=/workspace/cuDNN  -DTRT_LIB_DIR=$TRT_RELEASE/lib -DTRT_BIN_DIR=`pwd`/out   ..
 make -j4
 make install
 cd ../bin 
-./trtexec   --deploy=../samples/deploy.prototxt  --output=output
+./trtexec_debug    --deploy=../samples/deploy.prototxt  --output=output
