d65c2cb212e437b5f0c4009fc05cdeb53b5e0abf
renkuanze <renkuanze@sensetime.com>
2020-04-14 11:04:53 +0800

    为parseReshape添加注释

--- parsers/caffe/caffeParser/opParsers/parseReshape.cpp
+++ parsers/caffe/caffeParser/opParsers/parseReshape.cpp
@@ -32,6 +32,7 @@ ILayer* parseReshape(INetworkDefinition& network, const trtcaffe::LayerParameter
 
     const ::trtcaffe::BlobShape& shape = p.shape();
     // Check that N (batch dim) is 0. TensorRT does not support reshape in batch dimension
+    //  第一位必须是0,写成和输入维度相同的值也不行．
     if ((axis == 0) && (shape.dim(0) != 0))
     {
         std::cout << "Caffe Parser: Invalid reshape param. TensorRT does not support reshape in N (batch) dimension" << std::endl;
@@ -39,15 +40,37 @@ ILayer* parseReshape(INetworkDefinition& network, const trtcaffe::LayerParameter
     }
 
     // Handle axis and dims parameters
-    int axStart = std::max(0, axis - 1);
+    //  注意　nbDims是不包括batch这个维度的，默认为3
+    //  这代码有问题吧
+    // １．默认情况，没有axes和num_axes的参数，处理区间为[0,bottomDims.nbDims),正确
+    // ２．axis为正数，没有num_axes参数的情况． 逻辑比较奇怪，但是结果是正确的．（除了整除) 
+    // 3.　axis为负数，没有num_axes参数的情况．　完全不正确． TO DO
+    // 4. reshape param参数不够4个的情况
+    // int axStart = std::max(0, axis - 1);
+    int axStart = (axis >= 0) ? std::max(0, axis - 1):
+      bottomDims.nbDims + axis + 1;
+    //  这里axis-1的原因是，axis是按照caffe标准包含batch的，也就是0代表batch的维度
+    //  但是Dims里面却是不包括batch的．　因此axis=1其实是实际Dims中的第0维．　
+    // 　而axis=0的特殊情况 已经在　  if (axis == 0 && i == 0)　判掉了．
+
     int axEnd = p.has_num_axes() ? std::max(0, axis - 1 + p.num_axes()) : bottomDims.nbDims;
+
+    std::cout<<" axStart:"<< axStart << " axEnd:" << axEnd << std::endl;
     std::vector<int> reshapeDims;
 
-    reshapeDims.reserve(axStart);
+    // reshapeDims.reserve(axStart);
+    // // 不做reshape的维度，直接复制bottom的
     for (int i = 0; i < axStart; i++)
     {
         reshapeDims.push_back(bottomDims.d[i]);
     }
+
+    //  检查情况２
+    for (const auto & dim: reshapeDims)
+    {
+        std::cout<<" dim in reshapeDims:"<< dim << std::endl;
+    }
+
 
     for (int i = 0; i < shape.dim_size(); i++)
     {
@@ -59,13 +82,19 @@ ILayer* parseReshape(INetworkDefinition& network, const trtcaffe::LayerParameter
         if (shape.dim(i) == 0)
         {
             // If there is no bottom dimension corresponding to the current axis, then the params are invalid
-            assert(static_cast<int>(reshapeDims.size()) <= bottomDims.nbDims);
+            assert(static_cast<int>(reshapeDims.size()) < bottomDims.nbDims);
+            std::cerr<<" reshapeDims.size():"<<reshapeDims.size()<<" bottomDims.nbDims:"<<bottomDims.nbDims<<std::endl;
+            std::cerr<<" bottomDims.d[reshapeDims.size()]:"<< bottomDims.d[reshapeDims.size()] << std::endl;
             reshapeDims.push_back(bottomDims.d[reshapeDims.size()]);
         }
         else
         {
             reshapeDims.push_back(shape.dim(i));
         }
+    }
+    for (const auto & dim: reshapeDims)
+    {
+        std::cout<<"222: dim in reshapeDims:"<< dim << std::endl;
     }
 
     for (int i = axEnd; i < bottomDims.nbDims; i++)
@@ -75,6 +104,8 @@ ILayer* parseReshape(INetworkDefinition& network, const trtcaffe::LayerParameter
 
     Dims topDims{};
     topDims.nbDims = static_cast<int>(reshapeDims.size());
+
+    std::cout<< "bottomDims.nbDims:"<< bottomDims.nbDims << " topDims.nbDims:"<< topDims.nbDims << std::endl;
     for (int i = 0; i < topDims.nbDims; i++)
     {
         topDims.d[i] = reshapeDims[i];
@@ -89,8 +120,14 @@ ILayer* parseReshape(INetworkDefinition& network, const trtcaffe::LayerParameter
             countMinusOne += 1;
             // Inferred dimension
             int64_t newDim = parserutils::volume(bottomDims) / -parserutils::volume(topDims);
+            // 　都不考虑能不能整除的？？？
             topDims.d[i] = newDim;
         }
+    }
+
+    for (int i = 0; i < topDims.nbDims; i++)
+    {
+        std::cout<<"topDims.d[i]:" << topDims.d[i] << std::endl;
     }
 
     if (countMinusOne > 1)
